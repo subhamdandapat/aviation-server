a6367a5e6406b6d8cd9d385d5b72dbbbceed444d
Jyoti-meme <jyoti.mala@memeinfotech.com>
2020-10-13 18:45:27 +0530

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5002;
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 var mongoose = require('mongoose');
